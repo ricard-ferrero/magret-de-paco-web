dca82ea2dc5cfd35d660673cd84828aca00926c5
Ricard Ferrero <r.ferrero.conde@gmail.com>
2023-07-08 21:46:10 +0200

    carrousel working! :D

--- js/app.js
+++ js/app.js
@@ -1,48 +1,16 @@
-
-const carouselImgs = document.querySelectorAll('.header img');
-
-let index = 0;
-
-function nextCarouselImage() {
-    carouselImgs.forEach((element, i)=>{
-    i == index ? element.classList.remove('d-none') : element.classList.add('d-none');
+const carrousel = document.querySelectorAll('.img-container');
+let carrouselIndex = 1;
+function updateCarrousel() {
+    carrousel.forEach((imgContainer, index)=>{
+        if (index != carrouselIndex) {
+            imgContainer.classList.add('d-none');
+        } else {
+            imgContainer.classList.remove('d-none');
+        }
     });
-    index += 1;
-    if(index==carouselImgs.length) index=0;
-}
-
-setInterval(nextCarouselImage, 3000);
-
-/*
-const carousel = document.querySelector('.carousel-container');
-let position = 0;
-const width = carousel.clientWidth;
-
-function nextSlide() {
-    position -= width;
-    if (position < -width * (carousel.children.length - 1)) {
-    position = 0;
-    }
-    carousel.style.transform = `translateX(${position}px)`;
-}
-
-setInterval(nextSlide, 3000);
-*/
-
-/*
-const imageCarousel = document.querySelector('.header img');
-const imagesPaths = ['img/mdp.png', 'img/01.jpg'];
-let index = 0;
-
-function nextCarouselImage() {
-    imageCarousel.setAttribute('src', imagesPaths[index]);
-    index += 1;
-    if (index == imagesPaths.length) {
-    index = 0;
+    carrouselIndex += 1;
+    if (carrouselIndex >= carrousel.length) {
+        carrouselIndex = 0;
     }
 }
-
-nextCarouselImage();
-
-setInterval(nextCarouselImage, 3000);
-*/
+setInterval(updateCarrousel, 2000)
